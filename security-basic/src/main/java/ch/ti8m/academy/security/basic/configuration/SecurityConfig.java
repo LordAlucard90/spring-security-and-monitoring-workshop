5160dad43bdccd588a17e555c8122099da6829ae
Maggini Khaufra <kha@ti8m.ch>
2023-05-05 11:55:04 +0200

    added security exercises until roles assign to in memory user

--- security-basic/src/main/java/ch/ti8m/academy/security/basic/configuration/SecurityConfig.java
+++ security-basic/src/main/java/ch/ti8m/academy/security/basic/configuration/SecurityConfig.java
@@ -3,21 +3,56 @@ package ch.ti8m.academy.security.basic.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     private final String[] whiteList = new String[]{
-            "/messages/open"
+            "/messages/default/open",
     };
+    private final String ADMIN = "ADMIN";
+    private final String STAFF = "STAFF";
+    private final String USER = "USER";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().antMatchers(whiteList).permitAll();
+        http.authorizeHttpRequests()
+                .antMatchers(whiteList)
+                .permitAll();
+
+        http.httpBasic();
+
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+
         return http.build();
     }
 
-//    @Bean
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        var admin = User.withUsername("admin")
+                .password(encoder.encode("admin-password"))
+                .roles(ADMIN, STAFF, USER).build();
+        var staff = User.withUsername("staff")
+                .password(encoder.encode("staff-password"))
+                .roles(STAFF, USER).build();
+        var user = User.withUsername("user")
+                .password(encoder.encode("user-password"))
+                .roles(USER).build();
+        return new InMemoryUserDetailsManager(admin, staff, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer(WebSecurity web) {
 //        return (web) -> web.ignoring().antMatchers("/resources/**");
 //    }
--- security-basic/src/main/java/ch/ti8m/academy/security/basic/controller/MessageController.java
+++ security-basic/src/main/java/ch/ti8m/academy/security/basic/controller/MessageController.java
@@ -1,18 +1,47 @@
 package ch.ti8m.academy.security.basic.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("messages")
 public class MessageController {
     /*
         Accessible to anyone
-        test request on each user type -> Open
+        Exercise #1
      */
-    @GetMapping("open")
+    @GetMapping("default/open")
     public ResponseMessage open() {
         return new ResponseMessage("open to everyone");
+    }
+
+    /*
+        Accessible to any authenticated user
+        Exercise #2
+     */
+    @GetMapping("default/authenticated")
+    public ResponseMessage authenticated() {
+        return new ResponseMessage("open to any authenticated user");
+    }
+
+    /*
+        Accessible to anyone
+        test request on each user type -> Roles
+     */
+    @GetMapping("default/roles")
+    public ResponseMessage roles() {
+        var roles = SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return new ResponseMessage("The user has the following roles: " + roles);
     }
 }
